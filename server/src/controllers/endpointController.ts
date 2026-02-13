bbee47711b10335f5dc89c1f2322a130444edf5c
Pratik Survase <survasepratik1304@gmail.com>
2026-02-13 15:42:45 +0530

    completed backend for v1

--- server/src/controllers/endpointController.ts
+++ server/src/controllers/endpointController.ts
@@ -16,6 +16,33 @@ export const getEndpoints = async (
   }
 };
 
+export const getWebHooksById=async (req:Request, res:Response)=>{
+  try {
+     const {id}=req.params;
+     const endpointId = Number(id);
+     const limit=Number(req.query.limit)||20;
+     const page=Number(req.query.page)||1;
+     if(isNaN(endpointId)){
+      return res.status(400).json({message:"Invalid endpoint ID"});
+     }
+     const webhooks=await prisma.response.findMany({
+      where:{
+        endpointId:endpointId
+      },
+      orderBy:{
+        receivedAt:"desc"
+      },
+      take:limit,
+      skip:(page-1)*limit
+     })
+     console.log(webhooks);
+     res.status(200).json(webhooks);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch the webhooks:", error });
+    
+  }
+}
+
 export const generateToken = async (req: Request, res: Response) => {
   const maxRetries = 5;
   for (let i = 0; i < maxRetries; i++) {
--- server/src/routes/endpointRoutes.ts
+++ server/src/routes/endpointRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { generateToken, getEndpoints } from "../controllers/endpointController.js";
+import { generateToken, getEndpoints, getWebHooksById } from "../controllers/endpointController.js";
 
 const router=Router();
 
 router.get("/",getEndpoints);
+router.get("/:id/webhooks",getWebHooksById);
 router.post("/generate-token",generateToken);
 
 export default router;
